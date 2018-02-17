04cd26f1220b0ad59b053a7b823ab51adda60a2e
EDR\ebautista <ebautista@encorerepair.com>
2018-02-17 19:16:27 -0400

    Intial Commit

---
+++ Diary/Main.py
@@ -0,0 +1,40 @@
+import Contact as Contact
+
+
+class Main:
+
+	def add_contact():
+		self.contact.add(name,lastname,age)
+
+	def show_contact():
+		self.contact.show()
+
+
+	def show_menu():
+		print 'please choose one option:' 
+		print '1 - Add Contact' 
+		print '2 - Show contacts' 
+		choosed = raw_inpunt()
+
+		if choosed == 1:
+			add_contact()
+		elif
+			show_contact()
+
+
+	def __init_(self):
+		print 'Please type your name: '
+		name = raw_inpunt()
+		lastname = raw_inpunt()
+		print 'Please type your lastname: '
+		age = raw_inpunt()
+		print 'Please type your age: '
+
+		self.contact = Contact()
+		def show_menu()
+		
+
+
+	
+
+
