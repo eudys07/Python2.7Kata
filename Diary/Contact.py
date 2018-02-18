6efc7d7fea8c462e2c35163ab64df5cb2d76cba3
EDR\ebautista <ebautista@encorerepair.com>
2018-02-18 19:09:39 -0400

    initial commit

---
+++ Diary/Contact.py
@@ -0,0 +1,45 @@
+class Contact:
+
+	name = ''
+	lastname = ''
+	age = ''
+	contacs_list = []
+
+	def __init__(self, *att):
+
+		if len(att) > 0:
+			contact_data = list(att)
+
+			self.name = contact_data[0]
+			self.lastname = contact_data[1]
+			self.age = contact_data[2]
+		
+			print 'attributes well setted' 
+
+
+	def add(self, name, lastname, age):
+		
+		print 'attributes'
+		print name
+		print lastname
+		print age
+		contact_obj = {
+			'name': name,
+			'lastname': lastname,
+			'age': age
+		}
+
+		print 'Printing contact dict'
+		print contact_obj
+		self.contacs_list.append(contact_obj)
+
+
+
+	def show(self):
+
+		if len(self.contacs_list) <=0 :
+			print 'There is not contact to show'
+		else:
+			for contact in self.contacs_list:
+				print contact
+				print 'Full name:{0} {1} and age: {2}'.format(contact.name, contact.lastname, contact.age)
---
+++ Diary/Main.py
@@ -0,0 +1,57 @@
+from Contact import Contact 
+
+
+class Main:
+
+	def __init__(self):
+		self.contact = Contact()
+
+	def add_contact(self):		
+		self.ask_contact_info()
+		self.contact.add(self.contact.name, self.contact.lastname, self.contact.age)
+		print
+		print
+		print
+		self.show_menu()
+
+	def show_contact(self):
+		self.contact.show()
+		print
+		print
+		print
+		self.show_menu()
+
+	def ask_contact_info(self):
+		
+		print 'Please type your name: '
+		name = raw_input()
+		print 'Please type your lastname: '
+		lastname = raw_input()
+		print 'Please type your age: '
+		age = raw_input()
+
+		print 'Full name:{0} {1} and age: {2}'.format(name, lastname, age)
+		self.contact = Contact()
+		#self.contact = Contact(name, lastname, age)
+
+		
+
+	def show_menu(self):
+		print 'please choose one option:' 
+		print '1 - Add Contact' 
+		print '2 - Show contacts' 
+		choosed = raw_input()		
+
+		if choosed == '1':
+			self.add_contact()
+		elif choosed == '2':
+			self.show_contact()
+
+
+
+if __name__== "__main__":
+	 
+	main = Main()	
+	main.show_menu()
+	
+
