34a90f55a179a57803b840893ba756a35a76a6b8
EDR\ebautista <ebautista@encorerepair.com>
2018-03-05 20:10:46 -0400

    cleaning the conde based on 'Clean Code references'

--- Diary/Contact.py
+++ Diary/Contact.py
@@ -39,30 +39,20 @@ class Contact:
 		self.contacts.append(contact)
 
 
-
 	def show(self):
 		if self.contactsListHasValue() :
 			for contact in self.contacts:
-				print 'The Contact Id {0} named {1} {2} and age {3} is here'.format(contact['contact_id'], contact['name'], contact['lastname'], contact['age'])
+				print 'The Contact Id {0} named {1} {2} and age {3} '.format(contact['contact_id'], contact['name'], contact['lastname'], contact['age'])
 		else:
 			print 'There is not contact to show'
 			
 
-
 	def edit(self, edit_contact):
-		#self.contacts[] = contact
-		print 'edit contact: ', edit_contact
-		print 
-		print edit_contact.contact_id		
-		print self.contacts
 
 		for contact in self.contacts:
 			if contact['contact_id'] == edit_contact.contact_id :
-				print 'printing the contact in contact:', contact
-				print 'printing contact id:', contact['contact_id']
 				contact[edit_contact.edit_attribute] = edit_contact.new_value
 
-		print self.contacts
 
 	def delete(self, contact_id):
 		del self.contacts['contact_id']
@@ -71,8 +61,10 @@ class Contact:
 		print 
 		print contact
 
+
 	def getContatcSize(self):
 		return len(self.contacts)
+
 
 	def contactsListHasValue(self):
 		return self.contacts is not None and self.getContatcSize() > 0
--- Diary/ContactManager.py
+++ Diary/ContactManager.py
@@ -8,6 +8,52 @@ class ContactManager:
 	def __init__(self):
 		self.contact = Contact()
 		self.contact_edit = EditContact()
+		
+
+	def is_not_contacts_empty(self):
+		return self.contact.contactsListHasValue()
+
+
+	def suggest_add_contact_if_empty(self):
+		if self.is_not_contacts_empty():
+			print 'No contact has been added yet. Would you like to add one?'
+				print 'type [yes] to add otherwise press any key'
+				answer = raw_input()
+				if answer == 'yes':
+					self.add_contact()
+				else:
+					print 'contact list is empty'
+					print
+					print
+					self.show_menu()
+
+
+	def fill_edit_info(self):
+		print 'Please type the contact Id That You Would like to edit:'
+		self.contact_edit.contact_id = raw_input()
+		print 'What Would you like edit: [name] - [lastname] - [age]'
+		self.contact_edit.edit_attribute = raw_input()
+		print 'Which the value that you want to add in your ', self.contact_edit.edit_attribute
+		self.contact_edit.new_value = raw_input()
+
+
+	def edit_contact(self):		
+	
+		self.suggest_add_contact_if_empty()
+		self.fill_edit_info()
+		self.contact.edit(self.contact_edit)
+		print 
+		print 
+		self.show_menu()
+		
+
+	def delete_contact(self):
+		print 'Please type the contact Id That You Would like to delete:'
+		contact_id = raw_input()
+		if contact_id.isdigit() :
+			self.contact.delete(contact_id);
+		else:
+			print 'Please try to type a number instead'
 
 
 	def show_contacts(self):
@@ -23,62 +69,22 @@ class ContactManager:
 		self.show_menu()
 
 
-	def edit_contact(self):
-		
+	def select_menu_option(self):
 		try :
-			if self.contact.contactsListHasValue() is not True :
-				print 'No contact has been added yet. Would you like to add one?'
-				print 'type [yes] to add otherwise press any key'
-				answer = raw_input()
-				if answer == 'yes':
-					self.add_contact()
-				else:
-					self.show_menu()
+			user_choosed = raw_input()		
 
-			
-			print 'Please type the contact Id That You Would like to edit:'
-			self.contact_edit.contact_id = raw_input()
-			print 'What Would you like edit: [name] - [lastname] - [age]'
-			self.contact_edit.edit_attribute = raw_input()
-			print 'Which the value that you want to add in your ', self.contact_edit.edit_attribute
-			self.contact_edit.new_value = raw_input()
-
-			print 'contact id in edit ', self.contact_edit.contact_id
-			#self.contact_edit.contact_id = contact_edit_id
-
-			print 'printing contact_edit before call edit in contact'
-			print self.contact_edit
-
-			self.contact.edit(self.contact_edit)
-			print 
-			print 
-			self.show_menu()
+			if user_choosed == '1':
+				self.show_contacts()
+			elif user_choosed == '2':
+				self.add_contact()
+			elif user_choosed == '3':
+				self.edit_contact()
+			elif user_choosed == '4':
+				self.delete_contact()
 
 		except Exception as e:
 			raise ContactException(e)
 		
-
-	def delete_contact(self):
-		print 'Please type the contact Id That You Would like to delete:'
-		contact_id = raw_input()
-		if contact_id.isdigit() :
-			self.contact.delete(contact_id);
-		else:
-			print 'Please try to type a number instead'
-
-
-	def ask_contact_info(self):		
-		print 'Please type your ID: '
-		contact_id = raw_input()
-		print 'Please type your name: '	
-		name = raw_input()
-		print 'Please type your lastname: '
-		lastname = raw_input()
-		print 'Please type your age: '
-		age = raw_input()
-
-		print 'Full name:{0} {1} and age: {2}'.format(contact_id, name, lastname, age)
-		self.contact = Contact(contact_id, name, lastname, age)		
 
 
 	def show_menu(self):
@@ -92,15 +98,5 @@ class ContactManager:
 		print "----------------------------------------------"
 		print ">>Please choose your options"
 		print "----------------------------------------------"
-		user_choosed = raw_input()		
-
-		if user_choosed == '1':
-			self.show_contacts()
-		elif user_choosed == '2':
-			self.add_contact()
-		elif user_choosed == '3':
-			self.edit_contact()
-		elif user_choosed == '4':
-			self.delete_contact()
-
+		select_menu_option()
 
