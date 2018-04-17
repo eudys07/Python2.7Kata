31aa2281f397b958728e5cb012ec94f30a9b7bd5
EDR\ebautista <ebautista@encorerepair.com>
2018-04-17 07:28:18 -0400

    Implementing Dictionary

---
+++ Collections/Dictionary_implementation.py
@@ -0,0 +1,13 @@
+person = [{'name':'Eudys','lastname':'Bautista','age':'31'}]
+person.append({'name':'Aynel','lastname':'Bautista','age':'28'})
+#person.append({'name':'Eliel','lastname':'Garcia','age':'31'})
+
+print 'people: ', person
+
+
+for k, v in person.items:
+
+	print k
+	print v
+
+
