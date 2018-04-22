4f5376e999593cb20e07bced80306aeaf0fdb8df
ebautista <ebautista@encorerepair.com>
2018-04-22 11:05:33 -0400

    finishing list implementation and starting with tuples

--- Collections/list_implementation.py
+++ Collections/list_implementation.py
@@ -1,21 +1,43 @@
 x = []
 
+print 'type x'
 print type(x)
+print
+print
 
-#list concatenation
+print 'list concatenation'
 y = [1,2,3] + [4,5,6]
 y += [7,8]
+print
+print
 
-
-#Multiplying
+print 'Multiplying'
 x = [1,2,3,4] * 2
+print
+print
 
+print 'append'
 x.append(5)
 
 print 'X value: ', x
 print 'Y value: ', y
+print
+print
+
+print 'len'
 print 'size: ', len(x)
+print
+print
+
+print 'Checking membership'
+print '2 in x?'
 print 2 in x
+print '2 not in x?'
+print 2 not in x
+print
+print
+
+print 'count'
 print x.count(2)
 print x.count(5)
 
@@ -25,26 +47,26 @@ b = {'name':'Eliel'}
 c = {'name':'Aynel'}
 
 z = [a,b,c]
-
 print z
+print
+print
 
+print 'checking membership with object: '
 print 'a: in z: ', a in z
 
+print
+
+print 'checking membership with string: '
 print 'hin' in 'many things in the world'
 
-print 
-print 
-print 
-
+print
+print 'printing item from list'
+print
 for item in z:
+	print 'whole item:', item
+	print 'item attibute name: ', item['name']
 
-	print item
-	print item['name']
 
-
-print
-print
-print
 print
 print 'Minimun: ', min(x)
 print 'Maximun: ', max(x)
@@ -53,27 +75,30 @@ print 'Sum: ', sum(x)
 print 'Counting 2: ', x.count(2)
 print
 
-#unpacking
 
+print
+print 'unpacking'
 names = ['Eudys','Anderson','Bautista','Soto']
+print 'names: ', names
+print
 
 a, b, c, d = names
+print 'unpacking names in a,b,c,d: '
+print 'a: ', a
+print 'b: ', b
+print 'c: ', c
+print 'd: ', d
+
+
+print
+print
 obj1, obj2, obj3 = z
+print 'unpacking objects in z in obj1, obj2, obj3'
+print 'obj1: ', obj1
+print 'obj2: ', obj2
+print 'obj3: ', obj3
 
-
-
-print 'unpacking names print:'
-print a
-print b
-print c
-print d
-print 
-
-print 'unpacking z print:'
-print obj1
-print obj2
-print obj3
-
+print
 print
 print 'List Comprehension'
 #list Comprehension
@@ -97,7 +122,7 @@ print z
 
 print
 print
-hangar1 = [1,2,3,4]
+hangar1 = [1,'@',2,'@',3,4]
 hangar2 = [7,8,9]
 
 print
@@ -113,7 +138,36 @@ hangar2.insert(1,6)
 print 'hangar2: ', hangar2
 print 
 print 
-print 
 print 'Extending hangar2'
 hangar1.extend(hangar2);
 print 'hangar1 extend from hangar2: ', hangar1
+print 
+print 
+
+print 'using pop:' 
+hangar1.pop()
+print hangar1
+print 
+print 
+
+print 'using remove:' 
+hangar1.remove('@')
+print hangar1
+print
+print 'using remove again:' 
+hangar1.remove('@')
+print hangar1
+print 
+print 
+
+print 'using reverse:' 
+hangar1.reverse()
+print hangar1
+print 
+print 
+
+print 'using sort:' 
+hangar1.sort()
+print hangar1
+print 
+print 
---
+++ Collections/tuple_implementation.py
@@ -0,0 +1,9 @@
+#Tulples
+
+print 'Creating new tuple'
+print 'x = ()'
+x = ()
+print 'y = 1,2,3'
+y = 1,2,3
+print 'z = 1,'
+z = 1,
