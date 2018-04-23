70c2f3073e6a7fab83edb55795d4f2e505611791
ebautista <ebautista@encorerepair.com>
2018-04-23 07:49:04 -0400

    implementing set

---
+++ Collections/set_implementation.py
@@ -0,0 +1,22 @@
+#Set
+
+print 'Creating new set'
+print 'x = {}'
+x = {}
+print 'y = {1,2,3}'
+y = {1,2,3}
+print 'z = 1,'
+z = 1,
+print 'set(list1)'
+print
+print
+list1 = [1,2,3,4,3,2]
+set1 = set(list1)
+set2 = set(list1)
+
+
+print 'set1'
+print set1
+print 'set2'
+print set2
+
