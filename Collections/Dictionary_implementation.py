17ac20d9843409b538427951db7f0b9b08390e0b
ebautista <ebautista@encorerepair.com>
2018-04-29 08:44:42 -0400

    Finishing set implementation and starting with dictionaries

--- Collections/Dictionary_implementation.py
+++ Collections/Dictionary_implementation.py
@@ -1,13 +1,14 @@
-person = [{'name':'Eudys','lastname':'Bautista','age':'31'}]
-person.append({'name':'Aynel','lastname':'Bautista','age':'28'})
+person = {'name':'Eudys','lastname':'Bautista','age':'31'}
+people = [{'name':'Eudys','lastname':'Bautista','age':'31'}]
+people.append({'name':'Aynel','lastname':'Bautista','age':'28'})
 #person.append({'name':'Eliel','lastname':'Garcia','age':'31'})
 
-print 'people: ', person
+print 'people: ', people
+print
+print 'person type: ', type(person)
 
-
-for k, v in person.items:
-
-	print k
-	print v
+#for k, v in person.items:
+#	print k
+#	print v
 
 
