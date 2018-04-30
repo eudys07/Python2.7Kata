df82de1051ccb8c11514397e1e93cc154704d22b
ebautista <ebautista@encorerepair.com>
2018-04-30 09:37:29 -0400

    working with dict functions

--- Collections/Dictionary_implementation.py
+++ Collections/Dictionary_implementation.py
@@ -3,9 +3,9 @@
 print
 print 'Creating new Dictionary'
 print
-dict1 = {'name':'Eudys','lastname':'Bautista','age':'31'}
-dict2 = dict([('name','Aynel'),('lastname','Bautista'),('age','28')])
-dict3 = dict(name='Eliel',lastname='Garcia',age='32')
+dict1 = {'name':'Eudys','lastname':'Bautista','age':31}
+dict2 = dict([('name','Aynel'),('lastname','Bautista'),('age',28)])
+dict3 = dict(name='Eliel',lastname='Garcia',age=32)
 
 
 print 'dict1: '
@@ -15,8 +15,30 @@ print dict2
 print 'dict3: '
 print dict3
 
-#for k, v in 	.items:
-#	print k
-#	print v
+print
+print 'Dictionary operations: '
+print
+print
+
+print 'Adding or change item in dic2: '
+print 'dict2[age] = 29: '
+print dict2['age']
+dict2['age'] = 29
+print ''
+print 'dict2: ', dict2
+print
+
+print
+print 'deleting item in the list'
+del dict2['age']
+print ''
+print 'dict2: ', dict2
+print
+
+print
+print 'dict len'
+print ''
+print 'len(dict2): ', len(dict2)
+print
 
 
